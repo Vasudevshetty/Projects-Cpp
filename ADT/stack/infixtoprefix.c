306704c53dac4523e803b7f743c871fe83dc46c9
Vasudevshetty <vasudeepu2815@gmail.com>
2024-02-26 14:27:28 +0530

    Added postfix conversoin to the corresponding pgm

--- ADT/stack/infixtoprefix.c
+++ ADT/stack/infixtoprefix.c
@@ -18,52 +18,80 @@ int precdence(char c)
         return 0;
 }
 
-bool isOperand(char c)
+int isOperand(char c)
 {
     return (c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
 }
 
-void infixtoprefix(char infix[], char prefix[])
-{
+void infixToPostfix(char infix[], char postfix[]){
+    int i = 0, len = strlen(infix);
     int j = 0;
-    for (int i = strlen(infix) - 1; i >= 0; i--)
-    {
+
+    while(i < len){
         char token = infix[i];
-        if (isOperand(token))
-            prefix[j++] = token;
-        else if (token == ')')
+        if(isOperand(token))
+            postfix[j++] = token;
+        else if(token == '(')
             stack[++top] = token;
-        else if (token == '(')
-        {
-            while (top != -1 && stack[top] != ')')
-                prefix[j++] = stack[top--];
-            if (top != -1)
-                top--;
+        else if(token == ')'){
+            while(top != -1 && stack[top] != '(')
+                postfix[j++] = stack[top--];
+            top--; // to rmeove the (  opening parenthesis.
+        }else{
+            while(top!= -1 && precdence(token) <= precdence(stack[top]))
+                postfix[j++] = stack[top--];
+            
+            // if the encountered token is of highest priority.
+            stack[++top] = token;
         }
-        else
-        {
-            while (top != -1 && precdence(token) < precdence(stack[top]))
+        i++;
+    }
+    while(top!=-1)
+        postfix[j++] = stack[top--];
+    postfix[j] = '\0';
+}
+
+void infixToPrefix(char infix[], char prefix[]){
+    int i = 0, len = strlen(infix);
+    int j = 0;
+
+    for (i = len - 1; i >= 0; i--){
+        char token = infix[i];
+        if(isOperand(token))
+            prefix[j++] = token;
+        else if (token == ')') // if it is the opening parenthesis
+            stack[++top] = token;
+        else if(token == '('){ // fif it is the closing parenthesisi.
+            while(top != -1 && stack[top] != ')')
+                prefix[j++] = stack[top--];
+            top--;
+        }else{
+            while(top != -1 && precdence(token) < precdence(stack[top]))
                 prefix[j++] = stack[top--];
             stack[++top] = token;
         }
     }
-    while (top != -1)
+
+    while(top != -1)
         prefix[j++] = stack[top--];
+
     prefix[j] = '\0';
 
-    for (int i = 0, j = strlen(prefix) - 1; i < j; i++, j--)
-    {
+    for (i = 0, j = len - 1; i < j; i++, j--){
         char temp = prefix[i];
         prefix[i] = prefix[j];
         prefix[j] = temp;
     }
 }
 
+
 int main()
 {
-    char infix[100], prefix[100];
+    char infix[100], prefix[100], postfix[100];
     scanf("%s", infix);
-    infixtoprefix(infix, prefix);
-    printf("%s", prefix);
+    infixToPostfix(infix, postfix);
+    top = -1;
+    infixToPrefix(infix, prefix);
+    printf("%s %s", postfix, prefix);
     return 0;
 }
